a617483733ecffa06af1702fe8760140a3a18767
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    sdm632-common: add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- common.mk
+++ common.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
     libvulkan \
     libtinyxml
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
