4889e9ee8d9ee27fab34a03ca08b82a8048d350b
electimon <electimon@gmail.com>
2020-08-20 09:06:52 -0400

    sdm632-common: QTI PowerHAL is the default in CartelCAF

--- common.mk
+++ common.mk
@@ -373,7 +373,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service \
+    android.hardware.power@1.2-impl
 
 # QMI
 PRODUCT_PACKAGES += \
