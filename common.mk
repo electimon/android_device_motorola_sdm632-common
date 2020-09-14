0c2a270d3d905095e9afb21d650f91fe8d85d968
Arian <arian.kulmer@web.de>
2020-09-14 15:33:24 +0530

    sdm632-common: Load vndk 29 libprotobuf for libril-qc-hal-qmi and libwhvdil
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Change-Id: I14e2ceef71541003a452775e1dd7f44c8b3b2b5a

--- common.mk
+++ common.mk
@@ -471,8 +471,8 @@ PRODUCT_PACKAGES += \
     vndk_package
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
 # Weaver
 PRODUCT_PACKAGES += \
