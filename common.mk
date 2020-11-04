370ebb7aeb93a70f9c75e7fc89bf91c5d239d8ea
rjsh <rjsh@google.com>
2020-11-04 11:27:39 +0100

    sdm632-common: Enable v1.3 drm plugins Bug: 139134043 Test: MediaDrmTest Change-Id: I7453b035ca29c7ee889d03de0d55e6d947b3cb77

--- common.mk
+++ common.mk
@@ -164,7 +164,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
